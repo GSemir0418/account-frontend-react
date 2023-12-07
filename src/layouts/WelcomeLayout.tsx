c310ece51de858f299ad761fd095d9d01cd7f93f
gsemir0418 <845217811@qq.com>
2023-12-07 16:05:51 +0800

    feat: route transition

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import type { ReactNode } from 'react'
+import React, { useRef } from 'react'
+import { useLocation, useOutlet } from 'react-router-dom'
+import { animated, useTransition } from '@react-spring/web'
 
 interface Props {}
 export const WelcomeLayout: React.FC<Props> = () => {
-  return <div><Outlet /></div>
+  const outletCache = useRef<Record<string, ReactNode>>({})
+  const { pathname } = useLocation()
+  // 缓存 outlet 组件
+  const outlet = useOutlet()
+  outletCache.current[pathname] = outlet
+  const transitions = useTransition(pathname, {
+    // 进入态(welcome1没有进入态)
+    from: {
+      transform: pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)',
+    },
+    // 稳定态
+    enter: { transform: 'translateX(0%)' },
+    // 离开态
+    leave: { transform: 'translateX(-100%)' },
+    config: { duration: 500 },
+  })
+  return transitions((style, pathname) => {
+    return (
+      <animated.div style={style} key={pathname}>
+        {outletCache.current[pathname]}
+      </animated.div>
+    )
+  })
 }
